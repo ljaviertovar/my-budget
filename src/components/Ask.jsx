a9b170003fd5759f8e0134966faa3f03d1f1f79a
Luisbot <luis.tovar@cpavs.mx>
2021-01-14 22:12:45 -0600

    add: Aks and Error Components

---
+++ src/components/Ask.jsx
@@ -0,0 +1,56 @@
+import React, {Fragment, useState} from 'react'
+import Error from './Error'
+
+const Ask = () => {
+
+    const [quantity, setQuantity] = useState(0)
+    const [error, setError] = useState(false)
+
+    const defineBudget= e => {
+        setQuantity(parseInt(e.target.value, 10))
+    }
+
+    const addBudget = e => {
+        e.preventDefault()
+
+        if(quantity < 1 || isNaN(quantity)){
+            setError(true)
+            return
+        }
+
+
+        setError(false)
+    }
+
+
+    return (
+
+        <Fragment>
+
+            <h2>Insert your Budget</h2>
+
+            {error ? <Error  message="The Budget is incorrect" /> : null}
+
+            <form
+            onSubmit={addBudget}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Insert your Budget here"
+                    onChange={defineBudget}
+                />
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Define Budget"
+                />
+            </form>
+
+        </Fragment>
+
+    )
+
+}
+
+export default Ask
